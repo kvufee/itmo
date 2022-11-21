ec397016219a7336a055307c78910e3c33750374
wibbleydock <pzns1337@gmail.com>
2022-11-22 00:46:47 +0300

    [MODIFY] task3 and task5

---
+++ op/lab9_1s.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+
+int main()
+{
+    //task 3
+
+    int number;
+    scanf("%d", &number);
+
+    switch(number)
+    {
+
+        case 0:
+            printf("Zero");
+            break;
+
+        case 1:
+            printf("One");
+            break;
+
+        case 2:
+            printf("Two");
+            break;
+
+        case 3:
+            printf("Three");
+            break;
+        
+        case 4:
+            printf("Four");
+            break;
+
+        case 5:
+            printf("Five");
+            break;
+
+        case 6:
+            printf("Six");
+            break;
+
+        case 7:
+            printf("Seven");
+            break;
+
+        case 8:
+            printf("Eight");
+            break;
+        
+        case 9:
+            printf("Nine");
+            break;
+
+        default:
+            printf("Incorrect");
+            break;
+    }
+
+
+    //task 5
+
+    int deposit, percent, month, result;
+    scanf("%d %d %d", &deposit, &percent, &month);
+    
+    for (int i = 1; i < month + 1; ++i) 
+    {
+        result += result + result * (percent/100);
+        printf("%d", result);
+        printf("\n");
+    }
+
+    return 0;
+}
