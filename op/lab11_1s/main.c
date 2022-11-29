d75e76090e1f6ab4c72c6589b67bb0388bb8966c
wibbleydock <pzns1337@gmail.com>
2022-11-30 01:09:30 +0300

    [MODIFY] minor code fixes

--- op/lab11_1s/main.c
+++ op/lab11_1s/main.c
@@ -8,7 +8,6 @@ int main()
 
     scanf("%lf %lf", &rTriangle.x1, &rTriangle.y1);
     scanf("%lf %lf", &rTriangle.x2, &rTriangle.y2);
-    scanf("%lf %lf", &rTriangle.x3, &rTriangle.y3);
 
     printf("Perimeter = %.2lf \n", perimeter(rTriangle));
     printf("Squart = %.2lf \n", area(rTriangle));
--- op/lab11_1s/triangle.h
+++ op/lab11_1s/triangle.h
@@ -6,7 +6,6 @@ struct rightTriangle
 {
     float x1, y1;
     float x2, y2;
-    float x3, y3;
 };
 
 double area(struct rightTriangle);
