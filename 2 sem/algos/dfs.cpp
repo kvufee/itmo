994b035a9f68791276fe30cd8096407e9eeeedee
wibbleydock <pzns1337@gmail.com>
2023-03-16 00:02:08 +0300

    [MODIFY] minor code fixes and complete task

--- 2 sem/algos/dfs.cpp
+++ 2 sem/algos/dfs.cpp
@@ -2,59 +2,62 @@
 
 using namespace std;
 
-void dfs(vector <vector <long long>> &adjList, vector <long long> &visList, long long first, long long second)
+bool flag = false;
+
+void dfs(vector <vector <int>> &adjList, vector <int> &visList, int first, int second)
 {
     visList[first] = 1;
-    bool flag = false;
 
-    for (int i = 0; i < adjList.size(); ++i)
+    for (int i = 0; i < adjList[first].size(); ++i)
     {
-        if (visList[i] == 0)
-        {
-            dfs(adjList, visList, i, first);
-        } 
-        else if (visList[i] == 1 && first != second)
+        int vis = adjList[first][i];
+
+        if (second != vis && visList[vis] == 1)
         {
             flag = true;
+            return;
         }
+        else if (visList[vis] == 0)
+        {
+            dfs(adjList, visList, vis, first);
+        } 
     }
 
     visList[first] = 2;
 }
 
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-    bool flag = false;
-
-    long long n, m, first, second;
+    int n, m;
     cin >> n >> m;
 
-    vector <vector <long long>> adjList(n);
-    vector <long long> visList(n, 0);
+    vector <vector <int>> adjList(n + 1);
+    vector <int> visList(n + 1);
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < m; ++i)
     {
+        int first, second;
         cin >> first >> second;
         adjList[first].push_back(second);
         adjList[second].push_back(first);
     }
 
-    for (int i = 0; i < n; ++i)
+    for (int i = 1; i <= n; ++i)
     {
         if (visList[i] == 0)
         {
-            dfs(adjList, visList, i, first);
+            dfs(adjList, visList, i, 0);
         }
     }
 
     if (flag == true)
     {
         cout << "YES";
-    } else cout << "NO";
+        return 0;
+    } 
+    
+    cout << "NO";
 
     return 0;
 }
