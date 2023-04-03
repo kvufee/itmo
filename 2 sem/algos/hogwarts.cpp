ac0e53a42aa8d875cd696899e67e2d4b4011c6bf
wibbleydock <pzns1337@gmail.com>
2023-04-03 12:51:55 +0300

    [MODIFY] rework output and funcs

--- 2 sem/algos/hogwarts.cpp
+++ 2 sem/algos/hogwarts.cpp
@@ -5,21 +5,17 @@ using namespace std;
 bool flag = false;
 
 
-void dfs(vector <vector <int>> &adjList, vector <int> &visList, vector <int> currentComponent, int first, int second, int comp)
+void dfs(vector <vector <int>> &adjList, vector <int> &visList, int first, int comp)
 {
-    visList[first] = 1;
-    currentComponent[first] = comp;
+    visList[first] = comp;
 
     for (int i = 0; i < adjList[first].size(); ++i)
     {
-        int vis = adjList[first][i];
-        if (visList[vis] == 0)
+        if (visList[i] == false)
         {
-            dfs(adjList, visList, currentComponent, vis, first, comp);
-        } 
-    }
-
-    visList[first] = 2;
+            dfs(adjList, visList, first, comp);
+        }
+    } 
 }
 
 
@@ -30,8 +26,8 @@ int main()
 
     vector <vector <int>> adjList(n + 1);
     vector <int> visList(n + 1);
-    vector <bool> traps(n, false);
-    vector <int> currentComponent(n, -1);
+    vector <int> trapList(n + 1);
+    vector <bool> isTrap(comp + 1, 0);
 
     for (int i = 0; i < m; ++i)
     {
@@ -41,26 +37,40 @@ int main()
         adjList[second].push_back(first);
     }
     
-    for (int i = 1; i <= n; i++) {
-        char t;
-        cin >> t;
-        if (t == '1') {
-            traps[i] = true;
+    for (int i = 1; i <= n; ++i)
+    {
+        if (visList[i] == 0)
+        {
+            dfs(adjList, visList, i, ++comp);
         }
     }
 
     for (int i = 1; i <= n; ++i)
     {
-        int counter = 0;
-        if (visList[i] == 0)
+        cin >> trapList[i];
+    }
+    
+    for (int i = 1; i <= n; ++i)
+    {
+        if (trapList[i] == 0)
         {
-            dfs(adjList, visList, currentComponent, i, 0, comp);
-            ++counter;
-            ++comp;
+            isTrap[visList[i]] = isTrap[visList[i]] && (1 - trapList[i]) == 1;
         }
     }
 
-    
+    cout << comp << "\n";
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cout << visList[i] << " ";
+    }
+
+    cout << "\n";
+
+    for (int i = 1; i <= comp; ++i)
+    {
+        cout << (1 - isTrap[i]) << " ";
+    }
 
     return 0;
 }
