e74c9f09863adf55c7373cca5b1e83f4bc768d48
kvufee <pzns1337@gmail.com>
2023-04-26 17:21:15 +0300

    [MODIFY] add functions from deleted func.cpp file

--- 2 sem/cpp_basics/lab1/main.cpp
+++ 2 sem/cpp_basics/lab1/main.cpp
@@ -5,6 +5,32 @@
 using namespace std;
 
 
+//round v1
+void round_first(double& value)
+{
+    floor(value);
+}
+
+//round v2
+void round_second(double* value)
+{
+    floor(*value);
+}   
+
+
+//negate v1
+void negate_first(int& value)
+{
+    value * -1;
+}
+
+//negate v2
+void negate_second(int* value)
+{
+    *value * -1;
+}
+
+
 int main()
 {
     double value_double;
