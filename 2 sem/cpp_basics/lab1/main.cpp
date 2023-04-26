9c0d7322dcc4dd8b5eb166e0dd0c6163df6ffc49
kvufee <pzns1337@gmail.com>
2023-04-26 18:42:13 +0300

    [MODIFY] rework functions and add output for the first task

--- 2 sem/cpp_basics/lab1/main.cpp
+++ 2 sem/cpp_basics/lab1/main.cpp
@@ -1,45 +1,46 @@
+#include "header.h"
 #include <iostream>
 #include <cmath>
-#include "header.h"
 
 using namespace std;
 
 
 //round v1
-void round_first(double& value)
+void round_first(float& value)
 {
-    floor(value);
+    round(value);
 }
 
 //round v2
-void round_second(double* value)
+void round_second(float* value)
 {
-    floor(*value);
+    round(*value);
 }   
 
 
 //negate v1
 void negate_first(int& value)
 {
-    value * -1;
+    value *= -1;
 }
 
 //negate v2
 void negate_second(int* value)
 {
-    *value * -1;
+    *value *= -1;
 }
 
 
 int main()
 {
-    double value_double;
-    int value_int;
+    float value_first, value_second;
+    cin >> value_first >> value_second;
 
-    cin >> value_double >> value_int;
+    round_first(value_first);
+    round_second(&value_second);
 
-    round(&value_double);
-    round(value_double);
+    cout << "round reference: " << value_first << "\n";
+    cout << "round pointer: " << value_second << "\n";
 
     return 0;
 }
