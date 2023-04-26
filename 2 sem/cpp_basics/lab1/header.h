5a3313df18de063f5eac0ed58b1b458c213db0fa
kvufee <pzns1337@gmail.com>
2023-04-26 18:50:54 +0300

    [MODIFY] add square struct

--- 2 sem/cpp_basics/lab1/header.h
+++ 2 sem/cpp_basics/lab1/header.h
@@ -7,6 +7,14 @@ void round_second(float*);
 
 void negate_first(int&);
 void negate_second(int*);
+
+struct square
+{
+    int x1, y1;
+    int x2, y2;
+    int x3, y3;
+    int x4, y4;
+};
  
 
-#endif 
+#endif
