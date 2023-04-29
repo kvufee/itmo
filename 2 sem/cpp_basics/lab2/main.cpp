52ae76bfd8dc5fb161cb88b2ea5d61e2dd4e618d
kvufee <pzns1337@gmail.com>
2023-04-29 12:09:47 +0300

    [MODIFY] rework realisationand add output

--- 2 sem/cpp_basics/lab2/main.cpp
+++ 2 sem/cpp_basics/lab2/main.cpp
@@ -4,11 +4,22 @@
 using namespace std;
 
 
+squarePoly::squarePoly()
+{
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int x = 0;
+}
+
+squarePoly::squarePoly(int a, int b, int c, int x) : a(a), b(b), c(c), x(x) {}
+
+
 void squarePoly::curPointValue(squarePoly& debil)
 {   
     int answer;
 
-    answer = debil.getA() * pow(debil.getX(), 2) + debil.getB() * debil.getX() + debil.getC();
+    answer = debil.a * pow(debil.x, 2) + debil.b * debil.x + debil.x;
     
     cout << "current amount is: " << answer << "\n";
 }
@@ -17,7 +28,7 @@ void squarePoly::rootAmount(squarePoly& debil)
 {
     int discr, count;
     
-    discr = pow(debil.getB(), 2) - 4 * debil.getA() * debil.getC();
+    discr = pow(debil.b, 2) - 4 * debil.a * debil.c;
 
     if (discr == 0 || discr > 0)
     {
@@ -32,17 +43,17 @@ void squarePoly::findRoots(squarePoly& debil)
     int root1, root2;
     int discr;
 
-    discr = pow(debil.getB(), 2) - 4 * debil.getA() * debil.getC();
+    discr = pow(debil.b, 2) - 4 * debil.a * debil.c;
 
     if (discr == 0)
     {
-        root1 = -debil.getB() / (2 * debil.getA());
+        root1 = -debil.b / (2 * debil.a);
 
         cout << "x1 = x2 = " << root1 << "\n";
     } else if (discr > 0)
     {
-        root1 = (-debil.getB() + sqrt(discr)) / (2 * debil.getA());
-        root2 = (-debil.getB() - sqrt(discr)) / (2 * debil.getA());
+        root1 = (-debil.b + sqrt(discr)) / (2 * debil.a);
+        root2 = (-debil.b - sqrt(discr)) / (2 * debil.a);
 
         cout << "x1: " << root1 << "\n" << "x2: " << root2 << "\n";
     }
@@ -52,18 +63,18 @@ void squarePoly::findMinMax(squarePoly& debil)
 {
     int min_value, max_value, discr, vertex;
 
-    discr = pow(debil.getB(), 2) - 4 * debil.getA() * debil.getC();
+    discr = pow(debil.b, 2) - 4 * debil.a * debil.c;
 
     if (discr < 0)
     {
         cout << "No min/max" << "\n";
     } else if (discr == 0)
     {
-        vertex = -(debil.getB())/(2 * debil.getA());
+        vertex = -(debil.b)/(2 * debil.a);
 
         min_value = vertex;
 
-        max_value = debil.getA() * pow(vertex, 2) + debil.getB() * vertex + debil.getC();
+        max_value = debil.a * pow(vertex, 2) + debil.b * vertex + debil.c;
 
         cout << "min: " << min_value << "\n" << "max: " << max_value << "\n";
     }
@@ -72,9 +83,15 @@ void squarePoly::findMinMax(squarePoly& debil)
 
 int main()
 {
+    int cur;
     squarePoly SP;
 
-    
+    cin >> SP.a >> SP.b >> SP.c >> SP.x;
+
+    SP.curPointValue(SP);
+    SP.rootAmount(SP);
+    SP.findRoots(SP);
+    SP.findMinMax(SP);
 
     return 0;
 }
