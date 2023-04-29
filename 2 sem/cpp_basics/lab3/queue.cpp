8915dae0bcb170253af86eb924b2dd839865ac07
kvufee <pzns1337@gmail.com>
2023-04-29 15:43:38 +0300

    [MODIFY] start realisation

---
+++ 2 sem/cpp_basics/lab3/queue.cpp
@@ -0,0 +1,6 @@
+#include "queue.h"
+#include <iostream>
+
+using namespace std;
+
+
---
+++ 2 sem/cpp_basics/lab3/queue.h
@@ -0,0 +1,13 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+#define SIZE 100
+
+
+class qq
+{
+    public:
+    
+};
+
+
+#endif
