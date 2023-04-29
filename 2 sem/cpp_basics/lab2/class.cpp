fa2b5f74d035d8a50013c88a16af90f1737b60af
kvufee <pzns1337@gmail.com>
2023-04-29 10:25:31 +0300

    [MODIFY] add realisation from header.h

---
+++ 2 sem/cpp_basics/lab2/class.cpp
@@ -0,0 +1,29 @@
+#include "header.h"
+
+class squarePoly
+{
+    int a, b, c, x;
+
+    public:
+        squarePoly(int first, int second, int third, int value)
+        {
+            a = first;
+            b = second;
+            c = third;
+            x = value;
+        }
+
+        squarePoly(const squarePoly& debil)
+        {
+            a = debil.a;
+            b = debil.b;
+            c = debil.c;
+            x = debil.x;
+        }
+
+        int getA() { return a; }
+        int getB() { return b; }
+        int getC() { return c; }
+        int getX() { return x; }
+        
+};
