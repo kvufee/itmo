dad750630dcc3a8960513eb91e74cf61713084d3
kvufee <pzns1337@gmail.com>
2023-04-29 10:48:52 +0300

    [MODIFY] rename

---
+++ 2 sem/cpp_basics/lab1/header1.h
@@ -0,0 +1,36 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+ 
+#include <vector>
+
+
+void roundFirst(float&);
+void roundSecond(float*);
+
+
+void negateFirst(float&);
+void negateSecond(float*);
+
+
+struct square
+{
+    int x1, y1;
+    int x2, y2;
+    int x3, y3;
+    int x4, y4;
+};
+
+struct vec
+{
+    int x, y;
+};
+
+void moveSquareFirst(square &Square, vec &Vec);
+void moveSquareSecond(square *Square, vec *Vec);
+
+
+void mltpMatrixLinesFirst(vector<int> &matrix, int a);
+void mltpMatrixLinesSecond(vector<int> *matrix, int a);
+
+
+#endif
---
+++ 2 sem/cpp_basics/lab2/header2.h
@@ -0,0 +1,19 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+
+
+class squarePoly
+{
+    int a, b, c, x;
+
+    public:
+        squarePoly(int first, int second, int third, int value);
+
+        int getA();
+        int getB();
+        int getC();
+        int getX();
+        
+};
+
+#endif
