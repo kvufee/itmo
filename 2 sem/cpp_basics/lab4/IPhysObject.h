e141729863e976414f642c0e99d66c75c3cacd2e
wibbleydock <pzns1337@gmail.com>
2023-05-04 00:15:10 +0300

    [MODIFY] remove const type from parameter

--- 2 sem/cpp_basics/lab4/IPhysObject.h
+++ 2 sem/cpp_basics/lab4/IPhysObject.h
@@ -5,6 +5,6 @@ class IPhysObject
 {
     public:
         virtual double mass() = 0;
-        virtual bool operator== ( const IPhysObject& ob ) const = 0;
-        virtual bool operator< ( const IPhysObject& ob ) const = 0;
+        virtual bool operator== (IPhysObject& ob) const = 0;
+        virtual bool operator< (IPhysObject& ob) const = 0;
 };
