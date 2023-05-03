cbaa90194b121c636ec4f9ada37838caae5a0199
wibbleydock <pzns1337@gmail.com>
2023-05-03 19:39:44 +0300

    [MODIFY] add realisation of constructor

--- 2 sem/cpp_basics/lab4/main.cpp
+++ 2 sem/cpp_basics/lab4/main.cpp
@@ -4,13 +4,24 @@
 
 using namespace std;
 
-hexagon::hexagon()
-{
-};
+
+hexagon::hexagon() = default;
+
+hexagon::hexagon(const char* fig_name,
+                 unsigned int fig_size,
+                 double fig_square,
+                 double fig_perimeter,
+                 double fig_mass) : fig_name(fig_name),
+                                    fig_size(fig_size),
+                                    fig_square(fig_square),
+                                    fig_perimeter(fig_perimeter),
+                                    fig_mass(fig_mass) {}
+
+
 
 int main()
 {
-    hexagon hex;
+    hexagon hexagon;
 
     return 0;
 }
