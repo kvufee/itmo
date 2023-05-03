cf97713d7fed3b1c18ef4dd1fede3b9f461e2706
wibbleydock <pzns1337@gmail.com>
2023-05-03 17:41:41 +0300

    [MODIFY] rework whole file

--- 2 sem/cpp_basics/lab4/hexagon.cpp
+++ 2 sem/cpp_basics/lab4/hexagon.cpp
@@ -1,17 +1,27 @@
 #include "BaseCObject.h"
-#include "CVector2D.h"
 #include "IDialogInitiable.h"
 #include "IGeoFig.h"
 #include "IPhysObject.h"
 #include "IPrintable.h"
 
-#include <bits/stdc++.h>
 
-using namespace std;
-
-
-class hexagon : public BaseCObject, public CVector2D, public IDialogInitiable, public IGeoFig, public IPhysObject, public IPrintable
+class BaseCObject
 {
     public:
+        virtual const char* classname()
+        {
+            cin >> 
+        }
+};
 
-}
+class IDialogInitiable
+{
+
+};
+
+class IGeoFig
+{
+
+};
+
+class 
