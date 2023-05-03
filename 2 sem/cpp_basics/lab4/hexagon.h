eac28bed1ffa6d0c543e97671216e97ffb9bcd9f
wibbleydock <pzns1337@gmail.com>
2023-05-03 19:40:06 +0300

    [MODIFY] add class realisation

---
+++ 2 sem/cpp_basics/lab4/hexagon.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "BaseCObject.h"
+#include "IDialogInitiable.h"
+#include "IGeoFig.h"
+#include "IPhysObject.h"
+#include "IPrintable.h"
+
+
+class hexagon : public BaseCObject, public IDialogInitiable, public IGeoFig, public IPhysObject, public IPrintable
+{
+    public:
+        hexagon();
+
+        hexagon(const char* fig_name, unsigned int figure_size, double fig_square, double fig_perimeter, double fig_mass);
+
+
+        //BaseCObject
+        const char* classname() override;
+        unsigned int size() override;
+
+        //IDialogInitiable
+        void InitFromDialog() override;
+
+        //IGeoFig
+        double square() override;
+        double perimeter() override;
+
+        //IPhysObject
+        double mass() override;
+        bool operator== (const IPhysObject& ob);
+        bool operator< (const IPhysObject& ob);
+
+        //IPrintable
+        void draw() override;
+
+
+    private:
+        char str[100];
+        const char* fig_name;
+        unsigned int fig_size;
+
+        double fig_square, fig_perimeter;
+
+        double fig_mass;
+};
