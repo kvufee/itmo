6b920034ba8e2e07c031c770cd059ec400a29fa6
wibbleydock <pzns1337@gmail.com>
2023-05-03 17:58:38 +0300

    [MODIFY] create base classes and inherit class for correct overriding methods

--- 2 sem/cpp_basics/lab4/hexagon.cpp
+++ 2 sem/cpp_basics/lab4/hexagon.cpp
@@ -4,24 +4,72 @@
 #include "IPhysObject.h"
 #include "IPrintable.h"
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
 
 class BaseCObject
 {
     public:
-        virtual const char* classname()
-        {
-            cin >> 
-        }
+        virtual const char* classname();
+        virtual unsigned int size();
 };
 
 class IDialogInitiable
 {
+    public:
 
 };
 
 class IGeoFig
 {
+    public:
+        int square, perimeter;
+
+        double square()
+        {
+            cin >> square;
+        }
+
+        double perimeter()
+        {
+            cin >> perimeter;
+        }
 
 };
 
-class 
+class IPhysObject
+{
+    public:
+        double mass;
+
+        double mass()
+        {
+            cin >> mass;
+        }
+
+
+};
+
+class IPrintable
+{
+    public:
+
+};
+
+class hexagon : public BaseCObject, public IDialogInitiable, public IGeoFig, public IPhysObject, public IPrintable
+{
+    public:
+        const char* classname() override
+        {
+
+        }
+
+        unsigned int size() override
+        {
+            
+        }
+
+
+};
