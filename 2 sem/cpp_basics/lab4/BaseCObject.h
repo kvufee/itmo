4e65028e3c040a59346c7a5acb7a4a86f83e3b79
wibbleydock <pzns1337@gmail.com>
2023-05-03 19:39:01 +0300

    [MODIFY] minor code fixes

--- 2 sem/cpp_basics/lab4/BaseCObject.h
+++ 2 sem/cpp_basics/lab4/BaseCObject.h
@@ -3,12 +3,7 @@
 
 class BaseCObject
 {
-    public:
-        char str[100];
-
-        const char* fig_name = str;
-        unsigned int fig_size;
-        
-        virtual const char* classname() = 0;
-        virtual unsigned int size() = 0;
+public:
+    virtual const char* classname() = 0;
+    virtual unsigned int size() = 0;
 };
