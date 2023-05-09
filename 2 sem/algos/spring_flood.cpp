fbc303e4efbe679f84daf8b18ffc5ec8190be80f
wibbleydock <pzns1337@gmail.com>
2023-05-09 23:28:26 +0300

    [MODIFY] add 2 functions and start realisation in main

---
+++ 2 sem/algos/spring_flood.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int findParent(vector<int>& visList, int first)
+{
+    int result = 0;
+
+    if (visList[first] == first)
+    {
+        return first;
+    }
+    
+    visList[first] = findParent(visList, visList[first]);
+    result = visList[first];
+
+    return result;
+}
+
+void merge(vector<int>& visList, vector<int>& priorList, int first, int second)
+{
+    first = findParent(visList, first);
+    second = findParent(visList, second);
+
+    if (first == second)
+    {
+        return;
+    }
+
+    if (priorList[first] < priorList[second])
+    {
+        swap(first, second);
+    }
+
+    visList[second] = first;
+
+    if (priorList[first] == priorList[second])
+    {
+        ++priorList[first];
+    }
+}
+
+
+int main()
+{
+    int n, m, result = 0;
+    cin >> n >> m;
+
+    vector<tuple<int, int, int>> graph(m);
+    vector<int> visList(n + 1);
+    vector<int> priorList(n+1);
+    
+    for (int i = 0; i <= n; ++i)
+    {
+        visList[i] = i;
+        priorList[i] = 1;
+    }
+
+    for (int i = 0; i < m; ++i)
+    {
+        int first, second, weight;
+        cin >> first >> second >> weight;
+
+        graph[i] = make_tuple(first, second, weight);
+    }
+
+    sort(graph.begin(), graph.end());
+
+    for (int i = 0; i < m; ++i)
+    {
+        int first = get<0>(graph[i]);
+        int second = get<1>(graph[i]);
+        int weight = get<2>(graph[i]);
+
+
+    }
+
+
+
+    return 0;
+}
