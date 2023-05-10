ca486891f0d627e1d878dda76eb0aba79a94ab38
wibbleydock <pzns1337@gmail.com>
2023-05-11 00:44:54 +0300

    [MODIFY] add queue realisation

--- 2 sem/cpp_basics/lab3/queue.cpp
+++ 2 sem/cpp_basics/lab3/queue.cpp
@@ -4,3 +4,61 @@
 using namespace std;
 
 
+qq::qq()
+{
+    head = nullptr;
+    tail = nullptr;
+    next = nullptr;
+    data = 0;
+}
+
+qq::~qq()
+{
+    qq* temp = head;
+    while(temp != nullptr)
+    {   next = temp->next;
+        delete temp;
+        temp = next;
+    }
+}
+
+void qq::push(int value)
+{
+    auto* node = new qq;
+    node->data = value;
+    node->next = nullptr;
+    if(head == nullptr)
+    {
+        head = tail = node;
+    }
+    else
+    {
+        tail->next = node;
+        tail = node;
+    }
+}
+
+void qq::del() {
+    if(head == nullptr)
+    {
+        return;
+    }
+    qq* temp = head;
+    head = head->next;
+    delete temp;
+}
+
+qq& qq::operator<<(int value)
+{
+    qq::push(value);
+    return *this;
+}
+
+qq& qq::operator>>(int& value)
+{
+    if (head != nullptr) {
+        value = head->data;
+        del();
+    }
+    return *this;
+}
--- 2 sem/cpp_basics/lab3/queue.h
+++ 2 sem/cpp_basics/lab3/queue.h
@@ -1,9 +1,18 @@
 #pragma once
-#define SIZE 100
 
 
 class qq
 {
-    public:
-    
+public:
+    qq();
+    ~qq();
+    void push(int value);
+    void del();
+    qq& operator<<(int value);
+    qq& operator>>(int& value);
+private:
+    int data;
+    qq* head;
+    qq* tail;
+    qq* next;
 };
