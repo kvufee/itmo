21a1c7057fd5ed287c14e9c51bff8a1ba278c0be
wibbleydock <pzns1337@gmail.com>
2023-05-12 21:29:50 +0300

    [MODIFY] add template parameters

--- 2 sem/cpp_basics/lab5/array.cpp
+++ 2 sem/cpp_basics/lab5/array.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+
+template <class T, int n>
+T findMax(T arr[n])
+{
+    T ans = 0;
+    for (int i : arr)
+    {
+       if (arr[i] > ans)
+       {
+           ans = arr[i];
+       }
+    }
+
+    return ans;
+}
