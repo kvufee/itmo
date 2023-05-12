c5327e9107d423bd3389dcdcf9718e76eccc9e54
wibbleydock <pzns1337@gmail.com>
2023-05-12 22:31:34 +0300

    [MODIFY] add realisation

--- 2 sem/cpp_basics/lab5/main.cpp
+++ 2 sem/cpp_basics/lab5/main.cpp
@@ -1,3 +1,5 @@
+#include "array.cpp"
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -5,6 +7,17 @@ using namespace std;
 
 int main()
 {
+    int arr[] {12, 35, 66, 223, -300, -22};
+    cout << findMax<int, 6>(arr);
+
+    Array <int, 5> data;
+    data.add(5);
+    data.add(-3);
+    data.add(12);
+    data.del();
+    data.add(-222);
+    data.sortArr();
+
 
     return 0;
 }
