02d82bcafd214cf2e2e155ea11e00ec86ecde1f7
wibbleydock <pzns1337@gmail.com>
2023-05-18 21:47:11 +0300

    [MODIFY] add perimeter func realisation

--- 2 sem/cpp_basics/lab3/triangle.cpp
+++ 2 sem/cpp_basics/lab3/triangle.cpp
@@ -32,3 +32,18 @@ float triangle::heronSq(triangle& trg)
 
     return square;
 }
+
+float triangle::perimCount(triangle &trg)
+{
+    float perimeter;
+
+    float a, b, c;
+
+    a = pow((trg.x1 - trg.x2), 2) + pow((trg.y1 - trg.y2), 2);
+    b = pow((trg.x2 - trg.x3), 2) + pow((trg.y2 - trg.y3), 2);
+    c = pow((trg.x3 - trg.x1), 2) + pow((trg.y3 - trg.y1), 2);
+
+    perimeter = a + b + c;
+
+    return perimeter;
+}
