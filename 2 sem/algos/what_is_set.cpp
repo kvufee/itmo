0daf164a3b2bb7c5c8d6a4299a7162bc97197140
wibbleydock <pzns1337@gmail.com>
2023-05-19 17:23:40 +0300

    [MODIFY] the LAST rework

--- 2 sem/algos/what_is_set.cpp
+++ 2 sem/algos/what_is_set.cpp
@@ -3,29 +3,93 @@
 using namespace std;
 
 
+class HT
+{
+    long long size;
+    vector<list<long long>> hTable;
+
+public:
+    HT(long long TS) : size(TS)
+    {
+        hTable.resize(size);
+    }
+
+    long long hashFunc(long long temp) const
+    {
+        return temp % size;
+    }
+
+    void add(long long temp)
+    {
+        long long index = hashFunc(temp);
+        hTable[index].push_back(temp);
+    }
+
+    long long equal(const HT& htTemp) const
+    {
+        long long flag = 0;
+
+        if (size != htTemp.size)
+        {
+            return flag;
+        }
+
+        for (int i = 0; i < size; ++i)
+        {
+            if (hTable[i].size() != htTemp.hTable[i].size())
+            {
+                return flag;
+            }
+
+            for (int i = 0; i < hTable[i].size(); ++i)
+            {
+                bool fl = false;
+
+                for (int j = 0; j < htTemp.hTable[i].size(); ++j)
+                {
+                    if (i == j)
+                    {
+                        fl = true;
+                        break;
+                    }
+                }
+
+                if (fl == false)
+                {
+                    return flag;
+                }
+            }
+        }
+
+        flag = 1;
+        return flag;
+    }
+};
+
+
 int main()
 {
     int n;
     cin >> n;
 
-    const int c = INT_MAX;
+    HT set1(n + 1);
+    HT set2(n + 1);
 
-    int first, second;
-    int set1 = 0, set2 = 0;
+    long long first, second;
 
     for (int i = 0; i < n; ++i)
     {
         cin >> first;
-        set1 += first * INT_MAX;
+        set1.add(first);
     }
 
     for (int i = 0; i < n; ++i)
     {
         cin >> second;
-        set2 += second * INT_MAX;
+        set2.add(second);
     }
 
-    if (set1 == set2)
+    if (set1.equal(set2) == 1)
     {
         cout << "YES";
     } else cout << "NO";
