b994a27c2b3d7335801960cee191cc1b8bce7d2f
wibbleydock <pzns1337@gmail.com>
2023-05-20 16:37:54 +0300

    [MODIFY] replace struct with class

--- 2 sem/algos/bibaboba.cpp
+++ 2 sem/algos/bibaboba.cpp
@@ -3,7 +3,34 @@
 using namespace std;
 
 
-struct chairs
+class HT
+{
+    long long size;
+    vector<list<long long>> hTable;
+public:
+    HT(long long TS) : size(TS)
+    {
+        hTable.resize(size);
+    }
+
+    long long hashFunc(long long temp)
+    {   
+        if (temp < 0)
+        {
+            return size - abs(temp);
+        }
+
+        return temp % size;
+    }
+
+    void add(long long temp)
+    {
+        long long index = hashFunc(temp);
+        hTable[index].emplace_back(temp, 0);
+    }
+
+    
+};
 {
     vector<int> first;
     vector<int> second;
@@ -19,15 +46,15 @@ struct chairs
             int tmp;
             cin >> tmp;
 
-            if (i > tmp)
+            if (tmp < i)
             {
                 ans += first[i - tmp];
-                first[i - tmp]++;
+                ++first[i - tmp];
             }
             else
             {
                 ans += second[tmp - i];
-                second[tmp - i]++;
+                ++second[tmp - i];
             }
         }
 
@@ -38,6 +65,9 @@ struct chairs
 
 int main()
 {
+    int n;
+    cin >> n;
+
 
 
     return 0;
