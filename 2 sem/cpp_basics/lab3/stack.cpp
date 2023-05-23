c467deca0fabfa549611f148db66761283301d77
wibbleydock <pzns1337@gmail.com>
2023-05-23 21:41:09 +0300

    [MODIFY] add realisation for each function

--- 2 sem/cpp_basics/lab3/stack.cpp
+++ 2 sem/cpp_basics/lab3/stack.cpp
@@ -1,3 +1,82 @@
-//
-// Created by wibbleydock on 5/23/23.
-//
+#include "stack.h"
+
+#include <iostream>
+
+using namespace std;
+
+
+Stack::Stack()
+{
+    int head = -1;
+    int stack[100];
+}
+
+bool Stack::isEmpty()
+{
+    return head == -1;
+}
+
+bool Stack::isFull()
+{
+    return head == 99;
+}
+
+void Stack::add(int x)
+{
+    if (isFull())
+    {
+        cout << "Stack is full";
+        return;
+    }
+
+    ++head;
+    stack[head] = x;
+}
+
+int Stack::del()
+{
+    if (isEmpty())
+    {
+        cout << "Stack is empty";
+        return -1;
+    }
+
+    int temp = stack[head];
+
+    --head;
+
+    return temp;
+}
+
+int Stack::checkHead()
+{
+    return stack[head];
+}
+
+Stack& Stack::operator<<(int x)
+{
+    add(x);
+    return *this;
+}
+
+Stack& Stack::operator>>(int &x)
+{
+    x = del();
+    return *this;
+}
+
+void Stack::print(Stack s)
+{
+    if (s.isEmpty())
+    {
+        cout << "Stack is empty!";
+        return;
+    }
+
+    while (!s.isEmpty())
+    {
+        cout << s.del() << " ";
+    }
+
+    cout << '\n';
+}
