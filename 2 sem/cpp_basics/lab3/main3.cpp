46b86605f6d73d06f7730ab923c68cd9ef9326eb
wibbleydock <pzns1337@gmail.com>
2023-05-24 00:32:50 +0300

    [MODIFY] quick fix

--- 2 sem/cpp_basics/lab3/main3.cpp
+++ 2 sem/cpp_basics/lab3/main3.cpp
@@ -1,70 +1,65 @@
 #include "triangle.h"
-#include "queue.h"
+#include "stack.h"
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-
 int main()
 {
-    triangle trg1;
-    triangle trg2;
+    Stack stack;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        int val;
+        cin >> val;
+        stack.add(val);
+    }
+
+    stack.print(stack);
+
+    for (int i = 0; i < 5; ++i)
+    {
+        stack.del();
+    }
+
+
+    Triangle trg1;
+    Triangle trg2;
 
     cout << "triangle 1: ";
     cin >> trg1.x1 >> trg1.y1 >> trg1.x2 >> trg1.y2 >> trg1.x3 >> trg1.y3;
-    trg1.heronSq(trg1);
+    trg1.square = trg1.heronSq(trg1);
+    trg1.perimeter = trg1.perimCount(trg1);
 
-    cout << "Heron Square of 1st triangle: " << trg1.heronSq(trg1) << "\n";
-    
+    cout << "Heron Square of 1st triangle: " << trg1.square << "\n";
+    cout << "Perimeter of 1st triangle: " << trg1.perimeter << "\n";
+
     cout << "triangle 2: ";
     cin >> trg2.x1 >> trg2.y1 >> trg2.x2 >> trg2.y2 >> trg2.x3 >> trg2.y3;
-    trg2.heronSq(trg2);
+    trg2.square = trg2.heronSq(trg2);
+    trg2.perimeter = trg2.perimCount(trg2);
 
-    cout << "Heron Square of 2nd triangle: " << trg2.heronSq(trg2) << "\n";
+    cout << "Heron Square of 2nd triangle: " << trg2.square << "\n";
+    cout << "Perimeter of 1st triangle: " << trg2.perimeter << "\n";
 
     cout << "\n";
 
-    if (trg1.heronSq(trg1) == trg2.heronSq(trg2))
+
+    if (trg1 == trg2)
     {
         cout << "trg1 == trg2" << "\n";
-    } else if (trg1.heronSq(trg1) != trg2.heronSq(trg2))
+    } else if (trg1 != trg2)
     {
         cout << "trg1 != trg2" << "\n";
     }
 
-    if (trg1.heronSq(trg1) > trg2.heronSq(trg2))
+    if (trg1 > trg2)
     {
         cout << "trg1 > trg2" << "\n";
-    } else if (trg1.heronSq(trg1) < trg2.heronSq(trg2))
+    } else if (trg1 < trg2)
     {
         cout << "trg1 < trg2" << "\n";
-    }
-
-    /////////////////////////////////////////
-
-    int element, req_amount;
-    cin >> req_amount;
-
-    qq qq;
-
-    for(size_t i = 0; i < req_amount; ++i)
-    {
-        string command;
-        cin >> command;
-
-        if(command == "add")
-        {
-            cin >> element;
-            qq << element;
-        }
-
-        if(command == "del")
-        {
-            int val;
-            qq >> val;
-            cout << val << "\n";
-        }
     }
 
     return 0;
