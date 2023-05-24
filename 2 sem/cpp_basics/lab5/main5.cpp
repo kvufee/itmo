8ee4a60e6026d7e7dd679a17dfc268ad3b29580a
kvufee <pzns1337@gmail.com>
2023-05-24 16:10:06 +0300

    [MODIFY] add realisation

--- 2 sem/cpp_basics/lab5/main5.cpp
+++ 2 sem/cpp_basics/lab5/main5.cpp
@@ -1,23 +1,46 @@
-#include "array.cpp"
+#include "stack.h"
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
 
+template <typename T>
+T findMin(T a, T b)
+{
+    if (a < b)
+    {
+        return a;
+    } else return b;
+}
+
+
 int main()
 {
-    int arr[] {12, 35, 66, 223, -300, -22};
-    cout << findMax<int, 6>(arr);
+    Stack<int, 10> stack;
 
-    Array <int, 5> data;
-    data.add(5);
-    data.add(-3);
-    data.add(12);
-    data.del();
-    data.add(-222);
-    data.sortArr();
+    try
+    {
+        stack.add(1);
+        stack.add(5);
+        stack.add(12);
+        stack.add(-33);
+        stack.add(-1);
+        stack.add(11);
 
+        stack.del();
+        stack.del();
+    } catch (const overflow_error& error)
+    {
+        cout << error.what() << "\n";
+    } catch (const underflow_error& error)
+    {
+        cout << error.what() << "\n";
+    }
+
+
+    int a = 100, b = -3;
+    auto minval = findMin(a, b);
 
     return 0;
 }
