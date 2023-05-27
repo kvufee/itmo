53b9524897620f80822f6d8f62a709c313e33d62
kvufee <pzns1337@gmail.com>
2023-05-27 10:39:01 +0300

    [MODIFY] rework main

--- 2 sem/cpp_basics/lab4/main4.cpp
+++ 2 sem/cpp_basics/lab4/main4.cpp
@@ -1,14 +1,21 @@
 #include "hexagon.h"
 #include "ellips.h"
 
+#include <iostream>
+
+
 using namespace std;
 
 
 int main()
 {
+    cout << "Enter ellips" << "\n";
+
     ellips ellips;
     ellips.InitFromDialog();
     ellips.draw();
+
+    cout << "Enter hex" << "\n";
 
     hexagon hexagon;
     hexagon.InitFromDialog();
