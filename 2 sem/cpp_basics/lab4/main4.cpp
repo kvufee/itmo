e69caa75884315bce311dbdc907f7c7d24ac4d9e
kvufee <pzns1337@gmail.com>
2023-05-27 18:51:24 +0300

    [MODIFY] add structure figure

--- 2 sem/cpp_basics/lab4/main4.cpp
+++ 2 sem/cpp_basics/lab4/main4.cpp
@@ -1,14 +1,20 @@
 #include "hexagon.h"
 #include "ellips.h"
-
-#include <iostream>
-
+#include <bits/stdc++.h>
 
 using namespace std;
 
 
+struct Figure: public ellips, public hexagon
+{
+
+};
+
+
 int main()
 {
+    vector<Figure> contain;
+
     cout << "Enter ellips" << "\n";
 
     ellips ellips;
