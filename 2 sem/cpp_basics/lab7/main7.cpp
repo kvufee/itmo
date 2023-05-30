1e3f74efb3955cbe1cd6aa48418bffd6007d46ab
wibbleydock <pzns1337@gmail.com>
2023-05-31 02:44:57 +0300

    [MODIFDIFY] require fix for err 139

--- 2 sem/cpp_basics/lab7/main7.cpp
+++ 2 sem/cpp_basics/lab7/main7.cpp
@@ -1,20 +1,24 @@
 #include <bits/stdc++.h>
 
-#include "buffer.tpp"
-#include "buffer.hpp"
+#include "buffer.h"
+
 
 using namespace std;
 
 
 int main()
 {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
     CBuffer<int> c(4);
-    c.pushFront(1);
+    c.pushFront(13);
     c.pushFront(2);
-    c.pushFront(4);
-    c.pushFront(3);
-    cout << c[0] << c[1] << c[2] << c[3] << "\n";
+    c.pushFront(-3);
+    c.pushFront(25);
+
+    c.checkHead();
+    c.checkTail();
+
     sort(c.begin(),c.end());
+
+    c.checkHead();
+    c.checkTail();
 }
