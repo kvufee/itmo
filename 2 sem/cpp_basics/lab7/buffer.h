5a64196eb0eed911e0f7b591d3e10706af5fd490
wibbleydock <pzns1337@gmail.com>
2023-06-03 01:43:56 +0300

    [MODIFY] rework completely

--- 2 sem/cpp_basics/lab7/buffer.h
+++ 2 sem/cpp_basics/lab7/buffer.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -13,7 +13,135 @@ private:
     unsigned start;
     unsigned end_;
 public:
-    explicit CBuffer(unsigned capacity) :
+    class Iterator : public iterator<random_access_iterator_tag, T>
+    {
+    private:
+        T *s;
+    public:
+        Iterator(T *data)
+        {
+            s = data;
+        }
+
+        Iterator operator+(int x)
+        {
+            return Iterator(s+x);
+        }
+
+        T  operator-(CBuffer buf)
+        {
+            return s - buf.s;
+        }
+
+        Iterator operator-(int x)
+        {
+            return Iterator(s-x);
+        }
+
+        Iterator &operator+=(int x)
+        {
+            s += x;
+
+            return *this;
+        }
+
+        Iterator &operator-=(T x)
+        {
+            s -= x;
+
+            return *this;
+        }
+
+        Iterator &operator++()
+        {
+            ++s;
+
+            return *this;
+        }
+
+        Iterator &operator--()
+        {
+            --s;
+
+            return *this;
+        }
+
+        Iterator operator++(int)
+        {
+            Iterator tmp = *this;
+
+            ++(*this);
+
+            return tmp;
+        }
+
+        Iterator operator--(T)
+        {
+            Iterator t(*this);
+
+            --s;
+
+            return t;
+        }
+
+
+        T &operator*() const
+        {
+            return *s;
+        }
+
+        T *operator->() const
+        {
+            return *s;
+        }
+
+        T &operator[](const int x)
+        {
+            return s[x];
+        }
+
+        bool operator==(const Iterator &x) const
+        {
+            return x.s == s;
+        }
+
+        bool operator!=(const Iterator &x) const
+        {
+            return x.s != s;
+        }
+
+        bool operator<(const Iterator &x) const
+        {
+            return x.s < this->s;
+        }
+
+        bool operator>(const Iterator &x) const
+        {
+            return x.s > this->s;
+        }
+
+        bool operator<=(const Iterator &x) const
+        {
+            return x.s <= this->s;
+        }
+
+        bool operator>=(const Iterator &x) const
+        {
+            return x.s >= this->s;
+        }
+
+        T operator-(const Iterator &x)
+        {
+            return (s - x.s);
+        }
+
+        Iterator operator+(const Iterator &x)
+        {
+            return (s + x.s);
+        }
+    };
+
+    CBuffer(unsigned capacity) :
             buffer(new T[capacity]),
             size(capacity),
             current_size(0),
@@ -25,199 +153,104 @@ public:
         delete[] buffer;
     }
 
-    unsigned nextIndex(unsigned index) const
+    Iterator begin() const
     {
-        return (index + 1) % size;
+        return Iterator(buffer + start);
     }
 
-    unsigned prevIndex(unsigned index) const
+    Iterator end() const
     {
-        return (index + size - 1) % size;
+        return Iterator(buffer + end_);
     }
 
-    bool isEmpty() const
+    T operator[](int id)
     {
-        if (current_size == 0)
+        return buffer[id % size];
+    }
+
+    void pushFront(const T &x)
+    {
+        if (end_ == size)
         {
-            cout << "Stack is empty";
-            return current_size == 0;
-        }
-    }
+            buffer[start] = x;
 
-    bool ifFull() const
-    {
-        if (current_size == size)
-        {
-            cout << "Stack is full";
-            return current_size == size;
-        }
-    }
-
-    void pushFront(const T &value)
-    {
-        if (current_size == size)
-        {
-            popBack();
+            return;
         }
 
-        start = (start + size - 1) % size;
-        buffer[start] = value;
-        current_size++;
-    }
-
-    void pushBack(const T &value)
-    {
-        if (current_size == size)
+        for(int i = end_; i >= start; --i)
         {
-            popFront();
+            buffer[i + 1] = buffer[i];
         }
 
-        buffer[end_] = value;
-        end_ = (end_ + 1) % size;
-        current_size++;
+        buffer[start] = x;
+
+        ++end_;
+
+        return;
     }
 
     void popFront()
     {
-        if (isEmpty())
+        buffer[start] = 0;
+        ++start;
+    }
+
+    void pushBack(const T &x)
+    {
+        if (end_ == size)
         {
-            cout << "Buffer is empty\n";
-            return;
+            --end_;
         }
 
+        if (end_ == size + 1)
+        {
+            end_ -= 2;
+        }
 
-        start = (start + 1) % size;
-        current_size--;
+        buffer[end_] = x;
+
+        if (end_ != size + 1)
+        {
+            ++end_;
+        }
     }
 
     void popBack()
     {
-        if (isEmpty())
-        {
-            cout << "Buffer is empty\n";
-            return;
-        }
-
-        end_ = (end_ + size - 1) % size;
-        current_size--;
+        buffer[end_] = 0;
+        --end_;
     }
 
-    void checkHead()
+    void pushInSpecifiedPlace(Iterator curr, T val)
     {
-        if (isEmpty())
+        for(auto i = Iterator(buffer + end_); i != curr - 1; --i)
         {
-            cout << "Buffer is empty\n";
-            return;
+            *(i + 1) = *i;
         }
-
-        cout << buffer[start] << "\n";
+        *curr = val;
     }
 
-    void checkTail()
+    void removeByIndex(Iterator curr, T val)
     {
-        if (isEmpty())
+        for(auto i = curr; i != Iterator(buffer + start) - 1; ++i)
         {
-            cout << "Buffer is empty\n";
-            return;
+            *(i + 1) = *i;
         }
 
-        cout << buffer[end_ - 1] << "\n";
+        *curr = val;
     }
 
-    void checkSpecifiedPos(unsigned index)
+    void resize(const int &n)
     {
-        if (index >= current_size)
+        T *tmp = new T[n];
+
+        for (int i = 0; i < n; ++i)
         {
-            cout << "Index out of range\n";
-            return;
-        }
-
-        cout << buffer[(start + index) % size] << "\n";
-    }
-
-    void pushInSpecifiedPlace(unsigned index, const T &value)
-    {
-        if (index >= current_size)
-        {
-            cout << "Index out of range\n";
-            return;
-        }
-
-        unsigned current = prevIndex(start) + index;
-
-        if (current >= size)
-        {
-            current -= size;
-        }
-
-        if (current_size == size)
-        {
-            buffer[current] = value;
-        }
-        else
-        {
-            move_backward(buffer + current, buffer + prevIndex(end_), buffer + prevIndex(end_) + 1);
-            buffer[current] = value;
-            end_++;
-            current_size++;
-        }
-    }
-
-    void removeByIndex(unsigned index)
-    {
-        if (index >= current_size)
-        {
-            cout << "Index out of range\n";
-            return;
-        }
-
-        unsigned current = prevIndex(start) + index;
-
-        if (current >= size)
-        {
-            current -= size;
-        }
-
-        for (unsigned i = index + 1; i < current_size; i++)
-        {
-            unsigned next = prevIndex(start) + index;
-            buffer[current] = buffer[next];
-            current = next;
-        }
-
-        end_--;
-        current_size--;
-    }
-
-    void resize(unsigned new_size)
-    {
-        T *new_buffer = new T[new_size];
-
-        for (int i = 0; i < current_size; i++)
-        {
-            new_buffer[i] = buffer[(start + i) % size];
+            tmp[i] = i < size ? buffer[i] : 0;
         }
 
         delete[] buffer;
-        buffer = new_buffer;
-        size = new_size;
-        start = 0;
-
-        if (size < end_)
-        {
-            end_ = size;
-        }
-    }
-
-    typedef T *iterator;
-
-    iterator begin()
-    {
-        return iterator(&buffer[start]);
-    }
-
-    iterator end()
-    {
-        return iterator(&buffer[(end_ - 1 + size) % size]);
+        buffer = tmp;
+        size = n;
     }
 };
