a4b34eec7cf60c7494af4d884b6e18b65c6dc8f2
wibbleydock <pzns1337@gmail.com>
2024-02-18 00:48:14 -0500

    [MODIFY] finish task

--- 4 sem/java/src/lab4/ItemCatalog.java
+++ 4 sem/java/src/lab4/ItemCatalog.java
@@ -7,12 +7,75 @@ import java.util.HashMap;
 
 public class ItemCatalog
 {
-    private HashMap<Integer, Item> catalog = new HashMap<Integer, Item>();
-    private ArrayList<Item> ALCatalog = new ArrayList<Item>();
+    private final HashMap<Integer, Item> catalog = new HashMap<Integer, Item>();
+    private final ArrayList<Item> ALCatalog = new ArrayList<Item>();
 
     public void addItem(Item item)
     {
         catalog.put(item.ID, item);
         ALCatalog.add(item);
+    }
+
+    public void printItems()
+    {
+        for(Item i : ALCatalog)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public Item findItemById(int id)
+    {
+        if(!catalog.containsKey(id))
+        {
+            return null;
+        }
+        else
+        {
+            return catalog.get(id);
+        }
+    }
+
+    public Item findItemByIdArray(int id)
+    {
+        for(Item i : ALCatalog)
+        {
+            if(i.ID==id) return i;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+        ItemCatalog catalog = new ItemCatalog();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Item item = new Item();
+            item.ID = i;
+            catalog.addItem(item);
+        }
+
+
+        long begin1 = new java.util.Date().getTime();
+
+        for(int i = 0; i < 10000000; ++i)
+        {
+            catalog.findItemById(10);
+        }
+
+        long end1 = new java.util.Date().getTime();
+        System.out.println("HashMap: "+(end1-begin1));
+
+        long begin2 = new java.util.Date().getTime();
+
+        for(int i = 0; i < 10000000; ++i)
+        {
+            catalog.findItemByIdArray(10);
+        }
+
+        long end2 = new java.util.Date().getTime();
+        System.out.println("ArrayList: "+(end2-begin2));
     }
 }
