7e12d1138cd3795a3bbf6ee76f77b78f98978fd9
wibbleydock <pzns1337@gmail.com>
2024-03-16 12:37:48 -0400

    [MODIFY]

--- 4 sem/operationalSystems/lab1/file_name.bash
+++ 4 sem/operationalSystems/lab1/file_name.bash
@@ -10,22 +10,22 @@ if [ ! -f "$file_name" ]; then
 fi
 
 if [ -f "${file_name}.sh" ]; then
-    old_version=$(grep -oP '#Version \K.*' "${file_name}.sh")
-    new_version=$(grep -oP '#Version \K.*' "$file_name")
+    old_version=$(grep -oP '#Version: .*' "${file_name}.sh")
+    new_version=$(grep -oP '#Version: .*' "$file_name")
 
     if [[ "$new_version" > "$old_version" ]]; then
         echo "norm"
     else
-        echo "grustno."
+        echo "grustno"
         exit 1
     fi
 fi
 
 cp "$file_name" "${file_name}.sh"
-sed -i '/#TEMPLATE/d' "${file_name}.sh"
+sed -i '/#TEMPLATE/' "${file_name}.sh"
 
 if [ "$log_source" = "console" ]; then
     echo "norm"
 else
-    echo "norm balls" > install.log
+    echo "norm delaem" > install.log
 fi
