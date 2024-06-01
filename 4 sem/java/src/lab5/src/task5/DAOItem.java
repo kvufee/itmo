0d44fa16da7d771ad411fd80c1a3f313c0712885
wibbleydock <pzns1337@gmail.com>
2024-06-01 03:17:45 +0300

    [MODIFY]

--- 4 sem/java/src/lab5/src/task5/DAOItem.java
+++ 4 sem/java/src/lab5/src/task5/DAOItem.java
@@ -1,4 +1,39 @@
 package task5;
 
-public class DAOItem {
+import task3.Item;
+
+import java.sql.*;
+import java.math.BigDecimal;
+
+public class DAOItem
+{
+    private Connection connection;
+
+    public DAOItem(Connection connection)
+    {
+        this.connection = connection;
+    }
+
+    public void create(Item item)
+    {
+        String sql = "INSERT INTO ITEM (TITLE, RELEASEDATE, LISTPRICE, PRICE, VERSION) VALUES (?, ?, ?, ?, ?)";
+        try
+        {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setString(1, item.getTitle());
+            stmt.setDate(2, new java.sql.Date(item.getReleaseDate().getTime()));
+            stmt.setBigDecimal(3, new BigDecimal(String.valueOf(item.getPrice())));
+            stmt.setBigDecimal(4, new BigDecimal(String.valueOf(item.getPrice())));
+            stmt.setInt(5, 1);
+
+            int rowsInserted = stmt.executeUpdate();
+            System.out.println("Rows inserted: " + rowsInserted);
+
+            connection.commit();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
--- 4 sem/java/src/lab5/src/task5/DAOItemMain.java
+++ 4 sem/java/src/lab5/src/task5/DAOItemMain.java
@@ -1,4 +1,41 @@
 package task5;
 
-public class DAOItemMain {
+import task3.Item;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class DAOItemMain
+{
+    public static void main(String[] args)
+    {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String password = "postgres";
+        String user = "postgres";
+        Connection conn = null;
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(url, user, password);
+            conn.setAutoCommit(false);
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        var dao = new DAOItem(conn);
+        var searchdao = new task4.DAOItem(conn);
+
+        var newItem = new Item(0, "New Title", "tmp" , new Date(), new BigDecimal("239"), 1);
+
+        dao.create(newItem);
+
+        ArrayList<Item> items = searchdao.searchByKeyword("New Title");
+        System.out.println("Items with 'New Title' in the title: " + items);
+    }
 }
