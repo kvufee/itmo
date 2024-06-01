486b5b36c6cf6e8daa22c2e1485118e6323b288c
wibbleydock <pzns1337@gmail.com>
2024-06-01 03:05:16 +0300

    [MODIFY]

--- 4 sem/java/Lab5/src/task2/JDBCPostgresql.java
+++ 4 sem/java/Lab5/src/task2/JDBCPostgresql.java
@@ -1,4 +1,35 @@
 package task2;
 
-public class JDBCPostgresql {
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCPostgresql
+{
+    public static void main(String[] args)
+    {
+        String url = "jdbc:postgresql://localhost:6432/postgres";
+        String password = "postgres";
+        String user = "postgres";
+
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+
+            try (Connection conn = DriverManager.getConnection(url, user, password))
+            {
+                DatabaseMetaData meta = conn.getMetaData();
+
+                System.out.println("Driver name: " + meta.getDriverName());
+                System.out.println("Driver version: " + meta.getDriverVersion());
+                System.out.println("Product name: " + meta.getDatabaseProductName());
+                System.out.println("Product version: " + meta.getDatabaseProductVersion());
+            }
+        }
+        catch (ClassNotFoundException | SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
--- 4 sem/java/Lab5/src/task2/JDBCTest.java
+++ 4 sem/java/Lab5/src/task2/JDBCTest.java
@@ -1,4 +1,30 @@
 package task2;
 
-public class JDBCTest {
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCTest
+{
+    public static void main(String[] args)
+    {
+        String url = "jdbc:postgresql://localhost:6432/postgres";
+        String pwd = "postgres";
+        String name = "postgres";
+
+        try (Connection conn = DriverManager.getConnection(url, name, pwd))
+        {
+            DatabaseMetaData meta = conn.getMetaData();
+
+            System.out.println("Driver name: " + meta.getDriverName());
+            System.out.println("Driver version: " + meta.getDriverVersion());
+            System.out.println("Product name: " + meta.getDatabaseProductName());
+            System.out.println("Product version: " + meta.getDatabaseProductVersion());
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
